444cb663ec8376dccd7175b9e169aa0a554dbcd0
identitychris <Chris.Wolcott@nafinc.com>
2019-07-30 16:01:45 -0700

    python updates

--- pyg.py
+++ pyg.py
@@ -10,22 +10,22 @@ original = input('Enter a word: ')
 # Take the input Original variable and run it through the loop
 # Check to make sure the Original variable has characters and are only letters
 
+while True:
+    if len(original) > 0 and original.isalpha():
 
-if len(original) > 0 and original.isalpha():
+        # Take Original variable, make is all lower case and pass it to word variable
+        word = original.lower()
 
-    # Take Original variable, make is all lower case and pass it to word variable
-    word = original.lower()
+        # Take first letter of word variable, make new variable called first
+        first = word[0]
 
-    # Take first letter of word variable, make new variable called first
-    first = word[0]
+        # Combine the created variables to form a new concatenation string
+        new_word = word + first + pyg
 
-    # Combine the created variables to form a new concatenation string
-    new_word = word + first + pyg
+        # New variable to print, which just starts the new_word Variable at character 1 otherwise it prints out original WORD variable
+        new_word = new_word[1:len(new_word)]
 
-    # New variable to print, which just starts the new_word Variable at character 1 otherwise it prints out original WORD variable
-    new_word = new_word[1:len(new_word)]
-
-    print (new_word)
-
-else:
-    print ('That is not an acceptable word')
+        print (new_word)
+    else:
+        print ('That is not an acceptable word')
+    break
